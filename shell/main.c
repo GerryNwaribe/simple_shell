9ad2adbbe9e04c23d9de7d073a2cba091f195035
G33GII <gazasolomon@gmail.com>
2023-10-05 12:53:02 +0100

    Learned to format document on vscode

--- shell/main.c
+++ shell/main.c
@@ -5,8 +5,8 @@
  * @argv:
  * @env:
  * Return:
-*/
-int main (int _cxt, _dou_p argv, _dou_p env)
+ */
+int main(int _cxt, _dou_p argv, _dou_p env)
 {
     ssize_t _getline_Rv; /* Num of chars read from getline | can used to malloc the right size */
     size_t bffsz = 0;
@@ -19,18 +19,18 @@ int main (int _cxt, _dou_p argv, _dou_p env)
     while (X_OK)
     {
 
-/* ================== isatty == 1 for "It is interactive" ============= */
+        /* ================== isatty == 1 for "It is interactive" ============= */
         if (isatty(STDIN_FILENO))
             _print_string("[$] ");
 
-/* ===================== End_of_ISATTY | Start_of_Execve ============== */
+        /* ===================== End_of_ISATTY | Start_of_Execve ============== */
         _getline_Rv = getline(&bffr_Getline, &bffsz, stdin);
         if (_getline_Rv == ERROR)
         {
             free(bffr_Getline);
             return (-1);
         }
-/* ================== End_of_getline | Start_of_STRTOK ============== */
+        /* ================== End_of_getline | Start_of_STRTOK ============== */
 
         bffr_h = malloc(sizeof(char) * _getline_Rv);
         if (bffr_h == NULL)
@@ -42,15 +42,14 @@ int main (int _cxt, _dou_p argv, _dou_p env)
         _strcpy(bffr_h, bffr_Getline);
 
         token = strtok(bffr_h, _delimiters);
-        while (bffr_h != NULL)  /* Determining how many delimitable words there are */
+        while (bffr_h != NULL) /* Determining how many delimitable words there are */
         {
             token_idx++;
             token = strtok(NULL, _delimiters);
-                        
         }
         token_idx++;
 
-        argv  = malloc(sizeof(char*) * token_idx);
+        argv = malloc(sizeof(char *) * token_idx);
 
         token = strtok(bffr_h, _delimiters);
         for (token_idx = 0; token != NULL; token_idx++)
@@ -62,10 +61,7 @@ int main (int _cxt, _dou_p argv, _dou_p env)
         }
         argv[token_idx] = NULL;
 
-
-
-
- /* ================== End_of_STRTOK | Start_of_Execve ============= */
+        /* ================== End_of_STRTOK | Start_of_Execve ============= */
         _child_PID_Rv = fork();
         if (_child_PID_Rv == ERROR)
         {
@@ -83,17 +79,16 @@ int main (int _cxt, _dou_p argv, _dou_p env)
         {
             wait(_status);
         }
-/* ======================== End_of_Execve =========================== */
+        /* ======================== End_of_Execve =========================== */
 
-/* Freeing & Nullifying mallocs & Setting size = zero | Before next iteration */
+        /* Freeing & Nullifying mallocs & Setting size = zero | Before next iteration */
         free(bffr_h);
         bffr_h = NULL;
-        free(bffr_Getline);        
+        free(bffr_Getline);
         bffr_Getline = NULL;
-        //free(**argv)
+        // free(**argv)
         bffsz = 0;
     }
-
 
     /*free(bffr_h);
     free(bffr_Getline);*/
