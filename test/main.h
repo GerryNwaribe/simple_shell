af6dd271f96a2d60e4bdbaa0b6bd09a1d8b1e77d
G33GII <gazasolomon@gmail.com>
2023-10-05 21:12:55 +0100

    working on main.c

--- test/main.h
+++ test/main.h
@@ -9,8 +9,8 @@
 
 #define ERROR -1
 
-typedef char* string;
-typedef char** _dou_p;
+typedef char *string;
+typedef char **_dou_p;
 
 int _putchar(char c);
 int _print_string(string st);
@@ -20,11 +20,11 @@ int _print_string(string st);
 /**
  *  pid_t fork(void);
  *  int execve(const char *pathname, char *const argv[], char *const envp[]);
- * 
+ *
  * ========= wait, waitpid, waitid - wait for process to change state ============
  *  pid_t wait(int *wstatus);
  *  pid_t waitpid(pid_t pid, int *wstatus, int options);
  *  int waitid(idtype_t idtype, id_t id, siginfo_t *infop, int options);
  *              This is the glibc and POSIX interface;
  *                  see NOTES for information on the raw system call.
-*/
+ */
