39c07cfec1c16cc601c7afb8bd71e9a7da35323a
G33GII <gazasolomon@gmail.com>
2023-10-06 23:26:27 +0100

    modified:   ga_za/_execmd.c modified:   ga_za/a

--- ga_za/_execmd.c
+++ ga_za/_execmd.c
@@ -3,14 +3,28 @@
 /**
  * execmd -
  * @argv:
- * 
-*/
+ *
+ */
 void execmd(_dou_p local_argv, _dou_p env)
 {
-    int _status, _exev_Rv;
-    pid_t _child_PID_Rv;
 
-     _child_PID_Rv = fork();
+    /*int _status, _exev_Rv;
+    pid_t _child_PID_Rv;*/
+    string cmmd, atl_cmmd;
+    int _exev_Rv;
+
+    (void)env;
+    if (local_argv)
+    {
+        cmmd = local_argv[0];
+        atl_cmmd = NULL;
+
+        _exev_Rv = execve(atl_cmmd, local_argv, NULL);
+        if (_exev_Rv == -1)
+            perror("Error");
+    }
+
+    /* _child_PID_Rv = fork();
         if (_child_PID_Rv < 0)
         {
             perror("Fork");
@@ -20,12 +34,12 @@ void execmd(_dou_p local_argv, _dou_p env)
         }
         else if (_child_PID_Rv == 0)
         {
-            _exev_Rv = execve(local_argv[0], local_argv, env); /*Execution by the Child process*/
+            _exev_Rv = execve(local_argv[0], local_argv, env);          Execution by the Child process
             if (_exev_Rv == ERROR)
                 perror("Error");
         }
         else
         {
             wait(&_status);
-        }
+        }*/
 }
