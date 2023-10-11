c1dcf532b2e4a5b135556ce7f9092712c2bdfaab
G33GII <gazasolomon@gmail.com>
2023-10-11 14:51:27 +0100

    moving and merging files

---
+++ ga_za/T/ga_za/_exit_NUMBER.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _exit_num - Exits the Shell with an integer 'exit <N0>'
+ * @local_argv: The string to be converted into an integer.
+ */
+int _exit_num(_dou_p local_argv)
+{
+	int x;
+
+	if (local_argv[1])
+	{
+		for (x = 0; local_argv[1][x]; x++)
+		{
+			if (_isdigit(local_argv[1][x]))
+			{
+				continue;
+			}
+			else
+				exit(-1);
+		}
+
+		int _no = _atoi(local_argv[1]);
+
+		exit(_no);
+	}
+	exit(1);
+}
