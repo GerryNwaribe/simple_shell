6589c0a89ae5e674b50b2bcbecaa3692bd457731
G33GII <gazasolomon@gmail.com>
2023-10-18 09:03:00 +0100

    trying to handle leaks

--- _execmd.c
+++ _execmd.c
@@ -8,7 +8,53 @@
  * @line_num: line number
  * Return: void
  */
-void _execmd(dou_p local_argv, dou_p env, dou_p argv, size_t line_num)
+void execmd(dou_p local_argv, dou_p env, dou_p argv)
+{
+	int _exev_Rv, _status;
+	pid_t _child_PID_Rv;
+	string __addr = NULL;
+	char _addr[_BFFSZ];
+	char *_cmmd_PTR[_BFFSZ];
+
+	(void)env;
+	if (local_argv)
+	{
+		/*==================================*/
+		__addr = get_PATH(local_argv[0]);
+		strcpy(_addr, __addr);
+		_free(__addr, 0);
+		/*==================================*/
+		strcpy(_cmmd_PTR, local_argv);
+		_free(local_argv, 0);
+		/*==================================*/
+		if (_addr == NULL)
+		{
+			perror(argv[0]);
+			return;
+		}
+
+		_child_PID_Rv = fork();
+		if (_child_PID_Rv < 0)
+		{
+			perror(argv[0]);
+			_free(_cmmd_PTR, 0);
+			exit(EXIT_FAILURE);
+		}
+		else if (_child_PID_Rv == 0)
+		{
+			_exev_Rv = execve(_addr, _cmmd_PTR, env);
+			if (_exev_Rv == ERROR)
+			{
+				perror(argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+			wait(&_status);
+	}
+}
+
+/*void _execmd(dou_p local_argv, dou_p env, dou_p argv, size_t line_num)
 {
 	int _exev_Rv, _status;
 	pid_t _child_PID_Rv;
@@ -45,3 +91,4 @@ void _execmd(dou_p local_argv, dou_p env, dou_p argv, size_t line_num)
 			wait(&_status);
 	}
 }
+*/
--- _get_PATH.c
+++ _get_PATH.c
@@ -47,3 +47,54 @@ string _get_PATH(string cmmd)
 	}
 	return (NULL);
 }
+
+
+
+#include "main.h"
+
+/**
+ * get_PATH - This function gets all PATHs
+ *                          and determine the path for the command.
+ * @cmmd: The command from stdin who's path is to be found.
+ * Return: A pointer to valid PAth or NULL if it doesn't exist.
+ */
+string get_PATH(string cmmd)
+{
+	int dir_length, cmmd_length;
+	string path = NULL, _token = NULL, _addr = NULL;
+	char path_cp[_BFFSZ], file_path[_BFFSZ];
+	struct stat bffr;
+
+	path = getenv("PATH");
+	
+	if (path)
+	{		
+		_strcpy(path_cp, path);
+		cmmd_length = _strlen(cmmd);
+
+		_token = _strtok(path_cp, ":");
+		while (_token != NULL)
+		{
+			dir_length = _strlen(_token);
+			
+			_strcpy(file_path, _token);
+			_strcat(file_path, "/");
+			_strcat(file_path, cmmd);
+			_strcat(file_path, "\0");
+
+			if (!(stat(file_path, &bffr)))
+			{
+				_addr = malloc(cmmd_length + dir_length + 2);
+				strcpy(_addr, file_path);
+				return (_addr);
+			}
+
+			_token = _strtok(NULL, ":");
+		}
+
+		if (!(stat(cmmd, &bffr)))		/* /bin/ls */
+			return (cmmd);
+	}
+	return (NULL);
+}
+
