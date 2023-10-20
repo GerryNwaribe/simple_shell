e16009a90e2a58c165af50b3cd19abf0ae60d536
G33GII <gazasolomon@gmail.com>
2023-10-20 01:46:19 +0100

    final commit after making local checker task 1 all good

--- _isspace.c
+++ _isspace.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * _tokenization - Takes the string from stdin and tokenize into dou_p
- * @gtln: The string to be tokenized.
- * @_delimiters: The string of delimiters for parsing.
- * Return: A Double pointer.
- *
+ * _isblnk - Checks if a string contains only whitespace characters.
+ * @str: The string to be checked.
+ * Return: 1 if the string contains only whitespace characters, 0 otherwise.
  */
 int _isblnk(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i]; i++)
-        if (!isspace((unsigned char)str[i]))
-            return 0;   /* Found a non-whitespace character */
+	for (i = 0; str[i]; i++)
+		if (!isspace((unsigned char)str[i]))
+			return (0);   /* Found a non-whitespace character */
 
-    return 1;       /* All characters are whitespace */
+	return (1);   /* All characters are whitespace */
 }
